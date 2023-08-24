6bdc44f4d681b63a1d00b4d63a880fba094f681b
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-08-24 16:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let table = document.getElementsByTagName("table")[0];
 
 const getSum = () => {
-//Add your code here
-  
+    let sum = 0;
+    let priceArr = document.querySelectorAll(".price");
+
+    priceArr.forEach((singleEle) => {
+        sum += Number(singleEle.innerText);
+    })
+
+    let totalTR = document.createElement("tr");
+    totalTR.innerText = sum;
+
+    table.appendChild(totalTR);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
